36f2b067486094ad169a945f476df2c94232e445
Tri <trinm2102@gmail.com>
2025-10-18 01:07:32 +0700

    Reapply "cart"
    This reverts commit 55b0a2decea8075d1eaea9433e2d7b1280debb9b.

--- db.py
+++ db.py
@@ -27,3 +27,4 @@ def analyses_col(): return get_db().analyses
 def sessions_col(): return get_db().sessions
 def api_keys_col(): return get_db().api_keys
 def products_col(): return get_db().products
+def carts_col(): return get_db().carts
